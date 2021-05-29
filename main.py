43c9a40a88e4b300275a279c1017763c56d5e071
marvinamorim <marvinamorim@gmail.com>
2021-05-29 11:28:28 -0300

    adapt to work with my apex chat application

--- main.py
+++ main.py
@@ -6,69 +6,51 @@ import websockets
 
 logging.basicConfig()
 
-STATE = {"value": 0}
-
-USERS = set()
+USERS = []
 
 
-def state_event():
-    print("state change", STATE)
-    return json.dumps({"type": "state", **STATE})
-
-
-def users_event():
-    print("user in")
-    return json.dumps({"type": "users", "count": len(USERS)})
-
-
-async def notify_state():
+async def notify_users(message):
     if USERS:  # asyncio.wait doesn't accept an empty list
-        message = state_event()
-        await asyncio.wait([user.send(message) for user in USERS])
+        await asyncio.wait([user["websocket"].send(message) for user in USERS])
 
 
-async def notify_users():
-    if USERS:  # asyncio.wait doesn't accept an empty list
-        message = users_event()
-        await asyncio.wait([user.send(message) for user in USERS])
+async def register(websocket, user):
+    USERS.append({"websocket":websocket,"user":user})
+    message = {
+        "message":f"User {user} joined the chat.",
+        "type": "register",
+        "user": user
+    }
+    print(message, websocket)
+    await notify_users(json.dumps(message))
 
 
-async def register(websocket):
-    USERS.add(websocket)
-    print("register", websocket)
-    await notify_users()
-
-
-async def unregister(websocket):
-    USERS.remove(websocket)
-    print("unregister", websocket)
-    await notify_users()
+async def unregister(websocket, user):
+    USERS.remove({"websocket":websocket,"user":user})
+    message = {
+        "message":f"User {user} left the chat.",
+        "type": "unregister",
+        "user": user
+    }
+    print(message)
+    await notify_users(json.dumps(message))
 
 
 async def counter(websocket, path):
+    user = path[1:]
     # register(websocket) sends user_event() to websocket
-    await register(websocket)
+    await register(websocket, user)
     try:
-        await websocket.send(state_event())
+        #await websocket.send(state_event())
         async for message in websocket:
-            data = json.loads(message)
-            if data["action"] == "minus":
-                STATE["value"] -= 1
-                await notify_state()
-            elif data["action"] == "plus":
-                STATE["value"] += 1
-                await notify_state()
-            else:
-                logging.error("unsupported event: %s", data)
+            message_json = json.loads(message)
+            message_json["user"] = user
+            await notify_users(json.dumps(message_json))
     finally:
-        await unregister(websocket)
+        await unregister(websocket, user)
 
 
-start_server = websockets.serve(counter, None, 6789,
-create_protocol=websockets.basic_auth_protocol_factory(
-        realm="my dev server",
-        credentials=("hello", "iloveyou"),
-    ))
-
-asyncio.get_event_loop().run_until_complete(start_server)
-asyncio.get_event_loop().run_forever()
+if __name__ == '__main__':
+    start_server = websockets.serve(counter, None, 6789)
+    asyncio.get_event_loop().run_until_complete(start_server)
+    asyncio.get_event_loop().run_forever()
